e4f7f04e61ae8d21a468ade110f05ce2206e7de5
kleysonfmadruga <kleysonfmadruga26@gmail.com>
2021-07-07 11:37:38 -0300

    Adds the mapper for the Person and PersonDTO classes

---
+++ src/main/java/com/kleyson/personapi/mapper/PersonMapper.java
@@ -0,0 +1,16 @@
+package com.kleyson.personapi.mapper;
+
+import com.kleyson.personapi.dto.request.PersonDTO;
+import com.kleyson.personapi.entity.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface PersonMapper {
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO personDTO);
+    PersonDTO toDTO(Person person);
+}
